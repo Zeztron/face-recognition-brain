e81086b15a18dc6159c1793171098c63a06586af
Zeztron <pateltharsh@gmail.com>
2018-11-26 15:55:36 -0500

    Added the image recognition API

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css"
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return <div>
         <p className="f3">
           {
@@ -10,8 +10,8 @@ const ImageLinkForm = () => {
         </p>
         <div className="center">
           <div className="form center pa4 br3 shadow-5">
-            <input className="f4 pa2 w-70 center input" type="text" placeholder="Paste an image URL here..." />
-            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            <input className="f4 pa2 w-70 center input" type="text" placeholder="Paste an image URL here..." onChange={onInputChange}/>
+            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>
               Detect
             </button>
           </div>
