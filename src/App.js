b7b6088ac2dc8d06db6763e22db5c6d541762454
Zeztron <pateltharsh@gmail.com>
2018-11-26 16:25:43 -0500

    This is the coolest API ever!!!

--- src/App.js
+++ src/App.js
@@ -129,18 +129,20 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: ""
     }
   }
 
   onInputChange = (event) => {
-
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    this.setState({ imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function (response) {
         // do something with response
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function (err) {
         // there was an error
@@ -158,7 +160,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
