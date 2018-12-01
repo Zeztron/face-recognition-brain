314dbfdeb20ccc98c012c69a77c7d961967028e8
Zeztron <pateltharsh@gmail.com>
2018-12-01 18:14:20 -0500

    Added entries update from server to app

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ class App extends Component {
   loadUser = (data) => {
     this.setState({user: {
       id: data.id,
-      name: data.email,
+      name: data.name,
       email: data.email,
       entries: data.entries,
       joined: data.joined
@@ -185,7 +185,22 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if(response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
