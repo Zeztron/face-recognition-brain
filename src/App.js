ed6435b07efe8d7b9b9abc1224ab78b3f28d885d
Zeztron <pateltharsh@gmail.com>
2018-12-01 17:48:25 -0500

    added register from server to the app

--- src/App.js
+++ src/App.js
@@ -134,11 +134,26 @@ class App extends Component {
       imageUrl: "",
       box: {},
       route: "signin",
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: ""
+      }
     }
   }
 
-  
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.email,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -200,7 +215,7 @@ class App extends Component {
           : (
             route === "signin" 
               ? <Signin onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )
           
         }
