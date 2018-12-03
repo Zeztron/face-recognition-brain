27c86a60c5f5bccb77e24ec785d84d4637868ec3
Zeztron <pateltharsh@gmail.com>
2018-12-02 19:42:24 -0500

    Fixed a bug

--- src/App.js
+++ src/App.js
@@ -125,24 +125,26 @@ const particlesOptions = {
   retina_detect: true
 };
 
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: { },
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+}
+
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: ""
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -198,6 +200,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -206,7 +209,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === "home") {
       this.setState({isSignedIn: true})
     }
